d8f5b390e1d2d03870975f72c9eb0d0a2e57d14f
Felix <jonfelixrico@gmail.com>
2020-09-06 15:47:35 +0800

    Copy-paste common types from the core repository

---
+++ src/common/classes/errors/interactor-error.class.ts
@@ -0,0 +1,12 @@
+export default class InteractorError extends Error {
+  constructor(private errCode: InteractorErrorCodes) {
+    super()
+  }
+}
+
+export enum InteractorErrorCodes {
+  QUOTE_APPROVED = 'Quote Approved',
+  QUOTE_EXPIRED = 'Quote Expired',
+  QUOTE_NOT_FOUND = 'Quote Not Found',
+  NO_AVAILABLE_QUOTES = 'No Quotes Available',
+}
