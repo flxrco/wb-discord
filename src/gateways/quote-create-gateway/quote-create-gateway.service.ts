0185f3bb2c8beeb5301b883b0e5fb6bb0c60da85
Felix <jonfelixrico@gmail.com>
2020-09-07 22:31:14 +0800

    Create gateway for interacting with the quote submit interactor of wb-core

--- src/gateways/quote-create-gateway/quote-create-gateway.service.ts
+++ src/gateways/quote-create-gateway/quote-create-gateway.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import QuoteSubmitInteractor, {
+  ISubmitQuoteInput,
+  ISubmitQuoteOutput,
+  IApproveQuoteOutput,
+  IGetPendingQuotesOutput,
+} from 'src/common/core/classes/interactors/quote-submit-interactor.class'
+
+import MicroserviceMessages from 'src/common/core/enums/microservice-messages.enum'
+
+import { ClientProxy } from '@nestjs/microservices'
 
 @Injectable()
-export class QuoteCreateGatewayService {}
+export class QuoteCreateGatewayService extends QuoteSubmitInteractor {
+  constructor(private redis: ClientProxy) {
+    super()
+  }
+
+  async submitQuote(input: ISubmitQuoteInput): Promise<ISubmitQuoteOutput> {
+    return await this.redis
+      .send<ISubmitQuoteOutput>(MicroserviceMessages.SUBMIT_QUOTE, input)
+      .toPromise()
+  }
+
+  async approveQuote(messageId: string): Promise<IApproveQuoteOutput> {
+    return await this.redis
+      .send<IApproveQuoteOutput>(MicroserviceMessages.APPROVE_QUOTE, messageId)
+      .toPromise()
+  }
+
+  async getPendingQuotes(serverId: string): Promise<IGetPendingQuotesOutput> {
+    return await this.redis
+      .send<IGetPendingQuotesOutput>(
+        MicroserviceMessages.GET_PENDING_QUOTES,
+        serverId
+      )
+      .toPromise()
+  }
+}
