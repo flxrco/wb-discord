a8cd43c0d3541dbf83de21fc1bcc22edf3541caf
Felix <jonfelixrico@gmail.com>
2020-09-07 22:34:37 +0800

    Set up imports and exports of the gateways module

--- src/gateways/gateways.module.ts
+++ src/gateways/gateways.module.ts
@@ -1,7 +1,10 @@
-import { Module } from '@nestjs/common';
-import { QuoteCreateGatewayService } from './quote-create-gateway/quote-create-gateway.service';
+import { Module } from '@nestjs/common'
+import { QuoteCreateGatewayService } from './quote-create-gateway/quote-create-gateway.service'
+import { ProvidersModule } from 'src/providers/providers.module'
 
 @Module({
-  providers: [QuoteCreateGatewayService]
+  providers: [QuoteCreateGatewayService],
+  exports: [QuoteCreateGatewayService],
+  imports: [ProvidersModule],
 })
 export class GatewaysModule {}
