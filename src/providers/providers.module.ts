2dc36c65cc512da02ec7914bec5a18b020ab8872
Felix <jonfelixrico@gmail.com>
2020-09-09 18:27:23 +0800

    Remove code related to the moment provider

--- src/providers/providers.module.ts
+++ src/providers/providers.module.ts
@@ -3,10 +3,9 @@ import DiscordClientProvider from './discord-client.provider'
 import { Client } from 'discord.js'
 import { ClientProxy } from '@nestjs/microservices'
 import RedisClientProvider from './redis-client.provider'
-import MomentProvider, { MOMENT_PROVIDER } from './moment.provider'
 
 @Module({
-  providers: [DiscordClientProvider, RedisClientProvider, MomentProvider],
-  exports: [Client, ClientProxy, MOMENT_PROVIDER],
+  providers: [DiscordClientProvider, RedisClientProvider],
+  exports: [Client, ClientProxy],
 })
 export class ProvidersModule {}
