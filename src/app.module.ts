aaa94c13612b6dc376c363d9045fc4de372f6341
Felix <jonfelixrico@gmail.com>
2020-09-13 13:27:15 +0800

    Set up the Winston logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,36 @@
-import { Module } from '@nestjs/common'
+import { Module, Logger } from '@nestjs/common'
+import {
+  utilities as nestWinstonModuleUtilities,
+  WinstonModule,
+  WINSTON_MODULE_NEST_PROVIDER,
+} from 'nest-winston'
+import * as winston from 'winston'
 import { InteractorsModule } from './interactors/interactors.module'
 import { ServicesModule } from './services/services.module'
 import { ProvidersModule } from './providers/providers.module'
-import { ControllersModule } from './controllers/controllers.module';
-import { RepositoriesModule } from './repositories/repositories.module';
+import { ControllersModule } from './controllers/controllers.module'
+import { RepositoriesModule } from './repositories/repositories.module'
 
 @Module({
-  imports: [InteractorsModule, ServicesModule, ProvidersModule, ControllersModule, RepositoriesModule],
+  imports: [
+    InteractorsModule,
+    ServicesModule,
+    ProvidersModule,
+    ControllersModule,
+    RepositoriesModule,
+    WinstonModule.forRoot({
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            nestWinstonModuleUtilities.format.nestLike()
+          ),
+        }),
+        // other transports...
+      ],
+      level: 'debug',
+      // other options
+    }),
+  ],
 })
 export class AppModule {}
