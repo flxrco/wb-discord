4fb683c3b8c641e3120184ee4de56c6f166acd2f
Felix <jonfelixrico@gmail.com>
2020-09-13 14:31:24 +0800

    Apply minor changes to imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,5 @@
-import { Module, Logger } from '@nestjs/common'
-import {
-  utilities as nestWinstonModuleUtilities,
-  WinstonModule,
-  WINSTON_MODULE_NEST_PROVIDER,
-} from 'nest-winston'
+import { Module } from '@nestjs/common'
+import { utilities, WinstonModule } from 'nest-winston'
 import * as winston from 'winston'
 import { InteractorsModule } from './interactors/interactors.module'
 import { ServicesModule } from './services/services.module'
@@ -23,13 +19,11 @@ import { RepositoriesModule } from './repositories/repositories.module'
         new winston.transports.Console({
           format: winston.format.combine(
             winston.format.timestamp(),
-            nestWinstonModuleUtilities.format.nestLike()
+            utilities.format.nestLike()
           ),
         }),
-        // other transports...
       ],
       level: 'debug',
-      // other options
     }),
   ],
 })
