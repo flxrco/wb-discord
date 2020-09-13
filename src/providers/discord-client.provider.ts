6b0ceb5aa68000bfd959b3729ea00c0ae46996ff
Felix <jonfelixrico@gmail.com>
2020-09-13 13:51:34 +0800

    Make providers use the winston logger

--- src/providers/discord-client.provider.ts
+++ src/providers/discord-client.provider.ts
@@ -1,22 +1,26 @@
 import { Provider } from '@nestjs/common'
 import { Client } from 'discord.js'
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston'
+import { Logger } from 'winston'
 
 const { DISCORD_TOKEN } = process.env
 
 export default {
   provide: Client,
-  useFactory: () =>
+  useFactory: (logger: Logger) =>
     new Promise((resolve, reject) => {
+      logger = logger.child({ context: 'Discord' })
       const client = new Client()
       try {
-        console.log('Attempting to sign in.')
+        logger.info('Attempting to sign in.')
         client.login(DISCORD_TOKEN)
         client.on('ready', () => {
           resolve(client)
-          console.log('Sign in successful.')
+          logger.info('Sign in successful.')
         })
       } catch (e) {
         reject(e)
       }
     }),
+  inject: [WINSTON_MODULE_PROVIDER],
 } as Provider
--- src/providers/redis-client.provider.ts
+++ src/providers/redis-client.provider.ts
@@ -4,12 +4,15 @@ import {
   Transport,
 } from '@nestjs/microservices'
 import { Provider } from '@nestjs/common'
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston'
+import { Logger } from 'winston'
 
 const { MQ_URI } = process.env
 
 export default {
   provide: ClientProxy,
-  useFactory: () => {
+  useFactory: (logger: Logger) => {
+    logger.child({ context: 'Redis' }).info('Instantiated the Redis client.')
     return ClientProxyFactory.create({
       transport: Transport.REDIS,
       options: {
@@ -17,4 +20,5 @@ export default {
       },
     })
   },
+  inject: [WINSTON_MODULE_PROVIDER],
 } as Provider
