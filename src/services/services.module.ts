a31fd71b1bfe3111506e8dfb0a34bbe03744e994
Felix <jonfelixrico@gmail.com>
2020-09-16 20:59:37 +0800

    Remove GuildUtils

--- src/services/services.module.ts
+++ src/services/services.module.ts
@@ -1,19 +1,17 @@
 import { Module } from '@nestjs/common'
 import { ProvidersModule } from 'src/providers/providers.module'
-import { GuildUtilsService } from './guild-utils/guild-utils.service'
 import { ReactionsWatcherService } from './reactions-watcher/reactions-watcher.service'
 import { RepositoriesModule } from 'src/repositories/repositories.module'
 import { MessageWatcherService } from './message-watcher/message-watcher.service'
 import { CommandParserService } from './command-parser/command-parser.service'
 @Module({
   providers: [
-    GuildUtilsService,
     ReactionsWatcherService,
     MessageWatcherService,
     CommandParserService,
   ],
 
   imports: [ProvidersModule, RepositoriesModule],
-  exports: [GuildUtilsService, ReactionsWatcherService, CommandParserService],
+  exports: [ReactionsWatcherService, CommandParserService],
 })
 export class ServicesModule {}
