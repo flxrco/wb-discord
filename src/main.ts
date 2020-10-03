5f7638b6713f00753bdf1ab5daa119aca8d1d9f9
Felix <jonfelixrico@gmail.com>
2020-10-03 18:08:19 +0800

    Make the port configurable via environment variables

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import 'dotenv-defaults/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import 'dotenv-defaults/config'
+
+const { PORT } = process.env
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  await app.listen(PORT)
 }
 bootstrap()
